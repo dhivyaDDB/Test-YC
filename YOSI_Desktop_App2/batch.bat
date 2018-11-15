8531c2eb1b3ebc7fd4ac51fed51857f40040d05a
Dhivya <dhivya@dreamguys.co.in>
2018-11-15 15:09:04 +0530

    Batch file

--- YOSI_Desktop_App2/batch.bat
+++ YOSI_Desktop_App2/batch.bat
@@ -1,5 +1,5 @@
-cd D:\Automation_testing\YOSI_Desktop_App2
-set ProjectPath=D:\Automation_testing\YOSI_Desktop_App2
+cd C:\Users\Dreams\Jenkins\workspace\YC_Desktop\YOSI_Desktop_App2
+set ProjectPath=C:\Users\Dreams\Jenkins\workspace\YC_Desktop\YOSI_Desktop_App2
 echo %ProjectPath%
 set classpath=%ProjectPath%\bin;%ProjectPath%\lib\*
 echo %classpath%
