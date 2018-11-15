2a818710ad9d89a3bf46f7eab7e3ea7af632002c
Dhivya <dhivya@dreamguys.co.in>
2018-11-15 15:31:03 +0530

    batch file commit

--- YOSI_Desktop_App2/batch.bat
+++ YOSI_Desktop_App2/batch.bat
@@ -1,6 +1,4 @@
 cd C:\Users\Dreams\Jenkins\workspace\Batch_Test\YOSI_Desktop_App2
-set ProjectPath=C:\Users\Dreams\Jenkins\workspace\Batch_Test\YOSI_Desktop_App2
-echo %ProjectPath%
-set classpath=%ProjectPath%\bin;%ProjectPath%\lib\*
-echo %classpath%
-java org.testng.TestNG %ProjectPath%\testng.xml
+
+
+java -cp C:\Users\Dreams\Jenkins\workspace\Batch_Test\YOSI_Desktop_App2\lib*;C:\Users\Dreams\Jenkins\workspace\Batch_Test\YOSI_Desktop_App2\bin org.testng.TestNG testng.xml
